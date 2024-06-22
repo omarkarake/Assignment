0551161f190f3c9dcbc28eb9d83e62b1a8a17f2a
omar karake <omar.karake@amalitechtraining.org>
2024-06-22 13:24:17 +0200

    update on advice generator

--- week1/advice-generator/script.js
+++ week1/advice-generator/script.js
@@ -1,26 +1,39 @@
 const url = "https://api.adviceslip.com/advice";
 
 const getJson = async function (url, errorMessage = "Something Went Wrong") {
-  const response = await fetch(url);
-  if (!response.ok) throw new Error(`${errorMessage} (${response.status})`);
-  return await response.json();
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`${errorMessage} (${response.status})`);
+    return await response.json();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-let advice;
+// let advice;
 
 const quote = document.getElementById("quote");
 const quoteId = document.getElementById("quote-id");
 const nextQuote = document.getElementById("outer-select");
 
-const getJsonAdviceData = function (
+// const getJsonAdviceData = function (
+//   url,
+//   errorMessage = "Something Went Wrong"
+// ) {
+//   getJson(url).then((data) => {
+//     quoteId.innerText = data.slip["id"];
+//     advice = data.slip["advice"];
+//     quote.innerText = advice;
+//   });
+// };
+
+const getJsonAdviceData = async function (
   url,
   errorMessage = "Something Went Wrong"
 ) {
-  getJson(url).then((data) => {
-    quoteId.innerText = data.slip["id"];
-    advice = data.slip["advice"];
-    quote.innerText = advice;
-  });
+  const { slip } = await getJson(url);
+  quoteId.innerText = slip["id"];
+  quote.innerText = slip["advice"];
 };
 
 // getJson(url).then((data) => {
