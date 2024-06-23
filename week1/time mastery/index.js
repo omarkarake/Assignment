695d1d57471b0ce3c24e34c9b404af15a90d302e
omar karake <omar.karake@amalitechtraining.org>
2024-06-23 18:07:48 +0200

    Time Object basics

---
+++ week1/time mastery/index.js
@@ -0,0 +1,14 @@
+// 1. Time Object Basics
+
+// Create a Date object to represent the current time
+const currentDate = new Date();
+
+// Extract the current hour, minute, and second
+const currentHour = currentDate.getHours();
+const currentMinute = currentDate.getMinutes();
+const currentSecond = currentDate.getSeconds();
+
+// Log the current hour, minute, and second to the console
+console.log(`Current Time: ${currentHour}:${currentMinute}:${currentSecond}`);
+
+
